510fa8cc0d7737f43d87cb6d4dc9888c26db947b
ermin1990 <ermin1990@gmail.com>
2023-09-25 23:58:51 +0200

    f

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div className='container'><img src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader.gif"/></div>
+    <div className='container'><img src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader1.gif"/></div>
   )
 }
 
