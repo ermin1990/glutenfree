7e80b52321117bff8e3b8299dcfdf7d20e4f8d3b
ermin1990 <ermin1990@gmail.com>
2023-09-25 23:57:23 +0200

    fix loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div className='container'><img src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/loader.gif"/></div>
+    <div className='container'><img src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader.gif"/></div>
   )
 }
 
