88efb825e0d1e5dbb41966cbf84f764a0ea90469
ermin1990 <ermin1990@gmail.com>
2023-09-26 00:15:09 +0200

    fix: Loader dimensions

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div className='container'><img src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader1.gif"/></div>
+    <div className='container'><img className='loader' src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader1.gif"/></div>
   )
 }
 
