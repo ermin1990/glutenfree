a01446a19df11b32b7df4c96677ce09649f6dbc0
ermin1990 <ermin1990@gmail.com>
2023-10-11 21:48:38 +0200

    Feat: updated Abotu page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,39 @@
-import React from 'react'
 
-function About() {
+const About = () => {
   return (
-    <div className='p-2'>Kratko o meni</div>
-  )
-}
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://hubih.sfera.ba/wp-content/uploads/2022/11/1667389299719.jpg"
+            alt="Alma Bubić"
+            className="img-fluid rounded mb-2"
+          />
+        </div>
+        <div className="col-md-6">
+          <h2>O meni</h2>
+          <p>
+            Zdravo, ja sam Alma Bubić, a na Instagramu me možete pronaći kao @gospodjicasarlota.
+            Moja strast prema kulinarstvu, posebno slatkim kreacijama, vodi me kroz svakodnevnu
+            kulinarsku avanturu. Najveći deo svog vremena posvećujem pripremi slastica i istraživanju
+            novih recepata.
+          </p>
+          <p>
+            Posebno uživam u dekorisanju kolača i torti cvjećem i čokoladnim ukrasima.
+            Prvi koraci u svetu kuvanja slastica bili su u osnovnoj školi, a tokom godina, tokom
+            studija i raznih izazova u svetu bezglutenske ishrane, moja ljubav prema kulinarstvu
+            je rasla i usavršavala se.
+          </p>
+          <p>
+            Moja najveća inspiracija dolazi iz prirode i raznih kulinarskih tradicija koje volim
+            istraživati. Trenutno se fokusiram na kreaciju slastica koje su bezglutenske i
+            posebno namenjene svima koji dele istu strast prema ukusnoj hrani.
+          </p>
+          {/* Dodajte ovde preostali tekst o Almi */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
