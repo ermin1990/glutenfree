2acdac3c4b3c892294172e41332c333db13e20e4
ermin1990 <ermin1990@gmail.com>
2023-11-05 08:25:18 +0100

    Feat: Open Viber chat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import {BsFacebook, BsInstagram, BsTelephone} from 'react-icons/bs';
+import {FaViber} from 'react-icons/fa';
 import {MdSms} from 'react-icons/md';
 
 
@@ -12,6 +13,7 @@ const Footer = () => {
           <a className="nav-link" href="https://instagram.com/gospodjicasarlota" target="_blank" rel="noreferrer"><BsInstagram/> @gospodjicasarlota</a>
           <a className="nav-link" href="https://facebook.com/gospodjicasarlotaIIGlutenfree"><BsFacebook/> Gospođica Šarlota</a>
           <a className="nav-link" href="tel:061 946 958"><BsTelephone/>  061 946 958</a>
+          <a className="nav-link" href="viber://chat/?number=+38761946958"><FaViber/>  061 946 958</a>
           <a className="nav-link" href="sms:+387061946958;?&body=Napište koja vas torta zanima"><MdSms/> SMS 061 946 958</a>
           
         </div>
