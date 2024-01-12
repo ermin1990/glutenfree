a1950deeb34f214ba5df6833b3e7b75df69815a1
ermin1990 <ermin1990@gmail.com>
2024-01-12 13:37:09 +0100

    Logo link

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Loader() {
   return (
-    <div className='container p-1'><img className='loader' src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader1.gif" sizes={30} alt='loader'/></div>
+    <>
+    <div className='container p-1'><img className='loader' src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/cookieloader1.gif"  alt='loader'/></div>
+    <p>Učitavamo podatke, molimo vas sačekajte</p>
+    </>
+
   )
 }
 
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Logo() {
   return (
-    <div className="text-center mt-2"><img className="logo" src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/logo.png" alt="" /></div>
+    <div className="text-center mt-2"><NavLink to="/"><img className="logo" src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/logo.png" alt="" /></NavLink></div>
   )
 }
 
