c86772a6229d99ebad955db18360f6f275ff6949
ermin1990 <ermin1990@gmail.com>
2024-01-13 00:35:18 +0100

    Izmjena linka na Logo-u

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 function Logo() {
   return (
-    <div className="text-center mt-2"><NavLink to="/"><img className="logo" src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/logo.png" alt="" /></NavLink></div>
+    <>
+    <NavLink to="/">
+    <div className="text-center mt-2"><img className="logo" src="https://raw.githubusercontent.com/ermin1990/glutenfree/master/public/logo.png" alt="" /></div>
+    </NavLink>
+    </>
   )
 }
 
