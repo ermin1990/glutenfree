26601b2fc1dae424b2d547d8ddf660725f11d233
ermin1990 <ermin1990@gmail.com>
2024-09-04 14:57:45 +0200

    Update Torta.js
    Izmjenjen prikaz cijena

--- src/components/Torta.js
+++ src/components/Torta.js
@@ -3,24 +3,19 @@
 
 import { NavLink, useParams } from "react-router-dom";
 import Loader from "./Loader";
-
-
-
 import { useSelector } from "react-redux";
-
 import AlergenInfo from "./Utils/AlergenInfo";
 
 const Torta = () => {
   const { id } = useParams();
   const torta = useSelector((state) => state.data.data.Torte[id - 1]);
 
-
   const dimCijeneString = torta && torta.dimCijena;
   let dmCijene = [];
   if (dimCijeneString && dimCijeneString !== "null") {
-    dmCijene = dimCijeneString.split(";").map((item) => {
-      const [dimenzija, komada, cijena] = item.split("=");
-      return { dimenzija, komada, cijena };
+    dmCijene = dimCijeneString.split("/").map((item) => {
+      const [promjer, visina,  komada, cijena] = item.split(",");
+      return { promjer, visina,  komada, cijena };
     });
   }
 
@@ -43,41 +38,43 @@ const Torta = () => {
 
   return (
     <div className="p-2">
-      <h1 className="text-center text-uppercase font-weight-light text-balance ">
+      <h1 className="text-center text-uppercase font-weight-light text-balance">
         {torta.Naziv}
       </h1>
       <hr />
       <p className="mb-4">{torta.Opis}</p>
 
       <p className="text-uppercase font-weight-light">Cijene</p>
-      {/* Prikazivanje cijena ukoliko postoje */}
+      {/* Display prices if available */}
       {dmCijene.length > 0 && (
         <>
-          {dmCijene.map((dimenzijaCijena) => (
-            <div
-              key={dimenzijaCijena.dimenzija}
-              className="table cijenaTr m-1 p-2"
-            >
+          {dmCijene.map((dimenzijaCijena, index) => (
+            <div key={index} className="table cijenaTr p-2 torteLista">
               {dimenzijaCijena.cijena && (
-                <div className="d-flex justify-content-between">
+                <>
+                <div className="d-flex justify-content-between gap-2 align-items-center ">
                   <span>
-                    {`Promjer ${dimenzijaCijena.dimenzija} | Visina ${torta.visina} cm | ${dimenzijaCijena.komada} komada`}
+                    {`Promjer: ${dimenzijaCijena.promjer} cm`}<br/>
+                    {`Visina: ${dimenzijaCijena.visina} cm`}<br/>
+                    {`Komada: ${dimenzijaCijena.komada}`}
                   </span>
-                  <span className="cijenaText">
-                    {dimenzijaCijena.cijena} KM
-                  </span>
+  
+                  
+                  <span className="cijenaText veciFont">{dimenzijaCijena.cijena} KM</span>
+                  
                 </div>
+                </>
               )}
             </div>
           ))}
         </>
       )}
 
-      {/* Prikazivanje količina ukoliko postoje */}
+      {/* Display quantities if available */}
       {kolicina.length > 0 && (
         <>
-          {kolicina.map((item) => (
-            <div key={item.opis} className="table cijenaTr m-1 p-2">
+          {kolicina.map((item, index) => (
+            <div key={index} className="table cijenaTr m-1 p-2">
               {item.cijena && (
                 <div className="d-flex justify-content-between">
                   <span>{item.opis}</span>
@@ -89,13 +86,17 @@ const Torta = () => {
         </>
       )}
 
-      <AlergenInfo item={torta}/>
+      <AlergenInfo item={torta} />
 
-      <div className="bg-secondary p-2 rounded text-center text-color1 mt-2">Cijene su bez dekoracija</div>
+      <div className="bg-secondary p-2 rounded text-center text-color1 mt-2">
+        Cijene su bez dekoracija
+      </div>
 
-      <div className="bg-secondary p-2 rounded mt-3 ">
-      <h4 className="mt-1 text-center text-color1 ">Odaberi dekoraciju za tortu</h4>
-      <NavLink className="p-2 px-3 nav-link m-1 bg-color2 rounded text-center decoHover" to="/dekoracije">Dekoracije</NavLink>
+      <div className="bg-secondary p-2 rounded mt-3">
+        <h4 className="mt-1 text-center text-color1">Odaberi dekoraciju za tortu</h4>
+        <NavLink className="p-2 px-3 nav-link m-1 bg-color2 rounded text-center decoHover" to="/dekoracije">
+          Dekoracije
+        </NavLink>
       </div>
     </div>
   );
