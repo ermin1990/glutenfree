7d99ff01332ac0f5a2ff382daf0e42d03303fe49
erminselimovic <ermin1990@gmail.com>
2024-10-24 10:40:59 +0200

    update minitorta.js

--- src/components/MiniTorte/MiniTorta.js
+++ src/components/MiniTorte/MiniTorta.js
@@ -15,14 +15,14 @@ const Torta = () => {
   let dmCijene = [];
   if (dimCijeneString && dimCijeneString !== "null") {
     dmCijene = dimCijeneString.split("/").map((item) => {
-      const [promjer, visina,  komada, cijena] = item.split(",");
-      return { promjer, visina,  komada, cijena };
+      const [promjer, visina, komada, cijena] = item.split(",");
+      return { promjer, visina, komada, cijena };
     });
   }
 
   let singlPrice;
   if (dimCijeneString && dimCijeneString !== "null") {
-    singlPrice = dimCijeneString;    
+    singlPrice = dimCijeneString;
   }
 
   const kolicinaString = torta && torta.kolicina;
@@ -50,63 +50,62 @@ const Torta = () => {
       <hr />
       <p className="mb-4">{torta.Opis}</p>
 
-      
+
       {dmCijene.length > 0 ? (
-  <>
-    <p className="text-uppercase font-weight-light">Cijene</p>
-    {/* Prikaz cijena sa promjerom, visinom i komadima, ili samo cijena ako nema tih detalja */}
-    {dmCijene.map((dimenzijaCijena, index) => (
-      <div key={index} className="table cijenaTr p-2 torteLista">
-        {dimenzijaCijena.promjer && dimenzijaCijena.visina && dimenzijaCijena.komada ? (
-          <>
-            {/* Ako imamo detalje kao što su promjer, visina i komada */}
-            <div className="d-flex justify-content-between gap-2 align-items-center">
-              <span>
-                {`Promjer: ${dimenzijaCijena.promjer} cm`}<br/>
-                {`Visina: ${dimenzijaCijena.visina} cm`}<br/>
-                {`Komada: ${dimenzijaCijena.komada}`}
-              </span>
-              <span className="cijenaText veciFont">{dimenzijaCijena.cijena} KM</span>
+        <>
+          <p className="text-uppercase font-weight-light">Cijene</p>
+          {/* Prikaz cijena sa promjerom, visinom i komadima, ili samo cijena ako nema tih detalja */}
+          {dmCijene.map((dimenzijaCijena, index) => (
+            <div key={index} className="table cijenaTr p-2 torteLista">
+              {dimenzijaCijena.promjer && dimenzijaCijena.visina && dimenzijaCijena.komada ? (
+                <>
+                  {/* Ako imamo detalje kao što su promjer, visina i komada */}
+                  <div className="d-flex justify-content-between gap-2 align-items-center">
+                    <span>
+                      {`Promjer: ${dimenzijaCijena.promjer} cm`}<br />
+                      {`Visina: ${dimenzijaCijena.visina} cm`}<br />
+                      {`Komada: ${dimenzijaCijena.komada}`}
+                    </span>
+                    <span className="cijenaText veciFont">{dimenzijaCijena.cijena} KM</span>
+                  </div>
+                </>
+              ) : (
+                <>
+                  {/* Ako nemamo detalje o dimenzijama, prikaži samo raspon cijena */}
+                  <div className="d-flex justify-content-between">
+                    <span>Cijena</span>
+                    <span className="cijenaText veciFont">{singlPrice} KM</span>
+                  </div>
+                </>
+              )}
             </div>
-          </>
-        ) : (
-          <>
-            {/* Ako nemamo detalje o dimenzijama, prikaži samo raspon cijena */}
-            <div className="d-flex justify-content-between">
-              <span>Cijena</span>
-              <span className="cijenaText veciFont">{singlPrice} KM</span>
+          ))}
+        </>
+      ) : kolicina.length > 0 ? (
+        <>
+          {/* Prikaz samo cijena ako nema promjera, visine i komada */}
+          {kolicina.map((item, index) => (
+            <div key={index} className="table cijenaTr m-1 p-2">
+              {item.cijena && (
+                <div className="d-flex justify-content-between">
+                  <span>{item.opis}</span>
+                  <span className="cijenaText">{item.cijena} KM</span>
+                </div>
+              )}
             </div>
-          </>
-        )}
-      </div>
-    ))}
-  </>
-) : kolicina.length > 0 ? (
-  <>
-    {/* Prikaz samo cijena ako nema promjera, visine i komada */}
-    {kolicina.map((item, index) => (
-      <div key={index} className="table cijenaTr m-1 p-2">
-        {item.cijena && (
-          <div className="d-flex justify-content-between">
-            <span>{item.opis}</span>
-            <span className="cijenaText">{item.cijena} KM</span>
-          </div>
-        )}
-      </div>
-    ))}
-  </>
-) : (
-  <p className="text-center">Cijene trenutno nisu dostupne.</p>
-)}
+          ))}
+        </>
+      ) : (
+        <p className="text-center">Cijene trenutno nisu dostupne.</p>
+      )}
 
 
 
 
       <AlergenInfo item={torta} />
 
-    
 
+    </div>
   );
 };
-
 export default Torta;
